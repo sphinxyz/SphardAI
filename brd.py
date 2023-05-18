738f73de21d85b28935e978ae0af1d9bfea579d2
masrenda <tarenadiyaksa@gmail.com>
2023-05-18 22:36:48 +0700

    add: post method & translation process on the server side

--- brd.py
+++ brd.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from googletrans import Translator
 from Bard import Chatbot
 
 app = Flask(__name__, template_folder="./")
 
 token = open('./token.txt', "r").read().strip('\n').strip()
 bot = Chatbot(token)
+translator = Translator()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    output = bot.ask("who are u?")['content']
+    if request.method == 'POST':
+        command = request.form['command']
+        translated_command = translator.translate(command, src='id', dest='en').text
+        translated_output = bot.ask(translated_command)['content']
+        output = translator.translate(translated_output, src='en', dest='id').text
+    else:
+        output = ''
+
     return render_template('index.html', output=output)
 
 if __name__ == '__main__':
