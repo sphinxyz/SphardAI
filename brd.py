6a87e6856cc7441665813fd7ab04fadedaacc193
masrenda <tarenadiyaksa@gmail.com>
2023-05-20 12:03:32 +0700

    update: load environment variables from .env file

--- brd.py
+++ brd.py
@@ -1,24 +1,30 @@
 from flask import Flask, render_template, request
 from googletrans import Translator
+from dotenv import load_dotenv
 from Bard import Chatbot
+import os
 
 app = Flask(__name__, template_folder="./")
 
-token = open('./token.txt', "r").read().strip('\n').strip()
+load_dotenv()  # Load environment variables from .env file
+
+token = os.getenv("TOKEN")  # Get token from environment variable
+
 bot = Chatbot(token)
-translator = Translator()
+translator = Translator(service_urls=[
+      'translate.google.com',
+      'translate.google.co.kr',
+    ])
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    output = ''
     if request.method == 'POST':
         command = request.form['command']
         translated_command = translator.translate(command, src='id', dest='en').text
         translated_output = bot.ask(translated_command)['content']
         output = translator.translate(translated_output, src='en', dest='id').text
-    else:
-        output = ''
-
     return render_template('index.html', output=output)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, use_reloader=False)
